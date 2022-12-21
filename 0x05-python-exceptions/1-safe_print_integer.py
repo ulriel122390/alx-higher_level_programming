02b9f767c7444346b3da474d1cf4efe389e4a045
Sorel Ulriel <simosorel35@gmail.com>
2022-12-21 12:25:31 -0800

    tasks of the project<0x05-python-exceptions>

---
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+def safe_print_integer(value):
+    
+    try:
+        i = int(value)
+        print("{:d}".format(i))
+        return True
+    except:
+        return False
